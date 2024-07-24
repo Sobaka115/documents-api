9340d2dfcbf52ee50576683b40679a58d522c193
Maksim Petrov <petrovmp89@gmail.com>
2024-07-24 22:01:24 +0300

    Init document entity

--- src/documents/entities/document.entity.ts
+++ src/documents/entities/document.entity.ts
@@ -1,1 +1,10 @@
-export class Document {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Document {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  name: string;
+}
