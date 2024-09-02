df3eb6ec22e6a2814fbaa81de157db8e4aa3aefb
Maksim Petrov <petrovmp89@gmail.com>
2024-09-02 22:44:20 +0300

    update  startup script

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT, process.env.HOST);
+  console.info(
+    `Application started. Listening host: ${process.env.HOST}, port: ${process.env.PORT}`,
+  );
 }
 bootstrap();
