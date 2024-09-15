bdc0fba8a0ed1e403ad400e54978fb1dd46d4723
Maksim Petrov <petrovmp89@gmail.com>
2024-09-15 11:28:03 +0300

    add config schema and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,31 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DocumentsModule } from './documents/documents.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config as dotenvConfig } from 'dotenv';
+import { ConfigModule } from '@nestjs/config';
+import { configuration, CONFIG_SCHEMA } from './config';
 
-dotenvConfig({ path: '.env' });
+const {
+  database_host,
+  database_port,
+  database_username,
+  database_password,
+  database_name,
+} = configuration();
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema: CONFIG_SCHEMA,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: `${process.env.DATABASE_HOST}`,
-      port: Number(process.env.DATABASE_PORT),
-      username: `${process.env.DATABASE_USERNAME}`,
-      password: `${process.env.DATABASE_PASSWORD}`,
-      database: `${process.env.DATABASE_NAME}`,
+      host: database_host,
+      port: Number(database_port),
+      username: database_username,
+      password: database_password,
+      database: database_name,
       entities: ['dist/**/*.entity.js'],
       autoLoadEntities: true,
       synchronize: true,
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 
+import { configuration } from './config';
+
+const { host, port } = configuration();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT, process.env.HOST);
-  console.info(
-    `Application started. Listening host: ${process.env.HOST}, port: ${process.env.PORT}`,
-  );
+  console.info(`Application started. Listening host: ${host}, port: ${port}`);
 }
 bootstrap();
